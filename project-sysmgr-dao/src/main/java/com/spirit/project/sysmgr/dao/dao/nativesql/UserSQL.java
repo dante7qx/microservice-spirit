8045a43c5484a564ef150c0424769bb9b8eab6cf
dante <sunchao.0129@163.com>
2017-05-23 15:13:29 +0800

    代码规范修改

--- project-sysmgr-dao/src/main/java/com/spirit/project/sysmgr/dao/dao/nativesql/UserSQL.java
+++ project-sysmgr-dao/src/main/java/com/spirit/project/sysmgr/dao/dao/nativesql/UserSQL.java
@@ -8,6 +8,10 @@ package com.spirit.project.sysmgr.dao.dao.nativesql;
  */
 public final class UserSQL {
 	
+	private UserSQL() {
+		throw new IllegalAccessError("UserSQL 常量类，不能实例化！");
+	}
+	
 	// 根据userId删除用户角色表信息
 	public static final String DELETE_USER_ROLE_BY_USER_ID = "delete from t_user_role where user_id = ?1";
 	
